477a7258eb2b0ad108d70d5a21298b15e4be3344
AslanKral <aslandovletzade01@gmail.com>
2026-02-08 21:25:11 +0400

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,21 @@
+// sw.js faylının daxili
+const CACHE_NAME = 'TFTML';
+const urlsToCache = [
+  './',
+  './index.html',
+  './logo.jpg'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request)
+      .then((response) => response || fetch(event.request))
+  );
+});
